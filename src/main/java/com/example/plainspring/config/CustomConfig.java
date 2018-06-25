326d57d3918da5e07604ca66930de9a3dec8ae8f
Henri Prasetiya <henri.prasetiya@mitrais.com>
2018-06-25 09:57:47 +0800

    3rd

--- src/main/java/com/example/plainspring/config/CustomConfig.java
+++ src/main/java/com/example/plainspring/config/CustomConfig.java
@@ -2,16 +2,11 @@ package com.example.plainspring.config;
 
 import com.example.plainspring.repository.RepoOne;
 import com.example.plainspring.service.ServiceOne;
-import com.example.plainspring.service.ServiceTwo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomConfig {
-//    @Bean
-//    public ServiceTwo serviceTwo(){
-//        return new ServiceTwo();
-//    };
 
     @Bean
     public RepoOne repoOne(){
